c88390b0c599fd6825fac6afbf42bc6eff9b102a
Honor06 <hshha@hrasus2.localdomain>
2026-09-23 16:27:24 -0400

    Complete lab 03 scripting

--- src/ghevents/github-events.py
+++ src/ghevents/github-events.py
@@ -5,3 +5,25 @@ import requests
 
 GHUSER = os.getenv('GITHUB_USER')
 url = f'https://api.github.com/users/{GHUSER}/events'
+
+def retrieve_events(url):
+
+    """"downloads data from url resulting in a JSON text string which it passes to json.loads(...) and returns that Python object"""
+
+    request = requests.get(url).text
+    return json.loads(request)
+
+def print_events(events, n=5):
+    """"prints the first n events in the list in the form type :: repo"""
+    for x in events[:n]:
+         event = x['type'] + ' :: ' + x['repo']['name']
+    print(event)
+
+def main():
+    print(GHUSER)
+    print(url)
+    events = retrieve_events(url)
+    print_events(events)
+
+if __name__ == "__main__":
+    main()
